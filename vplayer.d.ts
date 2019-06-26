361da2f5931f635c2d731a0362198a019ff97588
Juan Pablo <juanpablocs21@gmail.com>
2019-06-26 09:20:37 -0500

    fix(vplayer): add typings

--- vplayer.d.ts
+++ vplayer.d.ts
@@ -5,7 +5,7 @@ declare module '*.scss';
 declare module '*.svg';
 
 
-interface ReactVPlayerProps {
+export interface ReactVPlayerProps {
     source: string|SourceProps[]
 }
 export default class VPlayer extends React.Component<ReactVPlayerProps> {
